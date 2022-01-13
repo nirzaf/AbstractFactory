cecf3aa241ca056346660eed3970c02031152aaf
nirzaf <mfmfazrin1986@gmail.com>
2022-01-13 16:20:56 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,74 @@
+﻿
+
+
+public interface IHotDrink
+{
+    void Consume();
+}
+
+public interface IHotDrinkFactory
+{
+    IHotDrink Prepare(int amount);
+}
+
+internal class TeaFactory : IHotDrinkFactory
+{
+    public IHotDrink Prepare(int amount)
+    {
+        Console.WriteLine($"Put int a tea bag boil water, pour {amount} ml, and add lemon, enjoy!" );
+        return new Tea();
+    }
+}
+
+internal class CoffeeFactory : IHotDrinkFactory
+{
+    public IHotDrink Prepare(int amount)
+    {
+        Console.WriteLine($"Grind  some beans and boil water, pour {amount}, add sugar & enjoy");
+        return new Coffee();
+    }
+}
+
+public class HotDrinkMachine
+{
+    public enum AvailableDrink
+    {
+        Coffee,
+        Tea
+    }
+
+    private Dictionary<AvailableDrink, IHotDrinkFactory> factories = new Dictionary<AvailableDrink, IHotDrinkFactory>();
+
+    public HotDrinkMachine()
+    {
+        foreach (AvailableDrink drink in Enum.GetValues(typeof(AvailableDrink)))
+        {
+            var factory = (IHotDrinkFactory) Activator.CreateInstance(
+                Type.GetType("AbstracyFactory."+ Enum.GetName(typeof(AvailableDrink), drink)+ "factory")
+                );
+
+            factories.Add(drink, factory);
+        }
+    }
+}
+
+
+internal class Tea : IHotDrink
+{
+    public void Consume()
+    {
+        Console.WriteLine("This tea is nice but I'd prefer it with milk");
+    }
+}
+
+internal class Coffee : IHotDrink
+{
+    public void Consume()
+    {
+        Console.WriteLine("This Coffee is sensational");
+    }
+}
+
+
+
+
